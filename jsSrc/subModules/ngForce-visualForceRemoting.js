35fe7242ca259e0af2899b8c2abce12c707836aa
Kevin Poorman <kjp@brightleafsoftware.com>
2014-12-01 16:53:41 -0500

    Massive fix for .send() based vfr methods
    This update bumps the version to 1.2 and restores funcationality I broke on 11/25 related to how .send() method works for VFR methods.

--- jsSrc/subModules/ngForce-visualForceRemoting.js
+++ jsSrc/subModules/ngForce-visualForceRemoting.js
@@ -41,175 +41,147 @@ angular.module('ngForce')
 				standardOpts = newOptions;
 			},
 			$get: function($q, $rootScope) {
-				return {
-					standardOptions: standardOpts,
-					/*
-					 * Kevin o'Hara released premote, a nice lib for wrapping
-					 * visualforce remoting calls in a promise interface. this
-					 * function .send() is largely a gentle refactoring of his
-					 * work, found in "premote" here:
-					 *    https://github.com/kevinohara80/premote
-					 * such that it locks into the ng exec loop and utilizes
-					 * the angular $q service, itself based on the Q lib
-					 * Kevin uses.
-					 */
-					/**
-					 * Returns a function that, when called, invokes the js
-					 * remoting method specified in this call.
-					 * @param  {String}   remoteAction class.methodName string representing the Apex className and Method to invoke
-					 * @param  {Object}   options      Ojbect containing at least the timeout and escaping options. Passed to Remoting call
-					 * @param  {Boolean}  nullok       Can this method return null and it be OK?
-					 * @return {Function}              Function engaged with the NG execution loop, making Visualforce remoting calls.
-					 */
-					send: function(remoteAction, options, nullok) {
-						var namespace, controller, method;
-						var Manager = Visualforce.remoting.Manager;
-						var parts = remoteAction.split('.');
-						if (options && typeof options !== 'object') {
-							throw new Error('Options must be an object');
-						}
-						if (parts.length < 2) {
-							throw new Error('Invalid Remote Action specified. Use Controller.MethodName or $RemoteAction.Controller.MethodName');
-						} else {
-							if (parts.length === 3) {
-								namespace = parts[0];
-								controller = parts[1];
-								method = parts[2];
-							} else if (parts.length === 2) {
-								controller = parts[0];
-								method = parts[1];
-							}
-						}
-
-						return function() {
-							var deferred = $q.defer();
-							var args;
-							if (arguments.length) {
-								args = Array.prototype.slice.apply(arguments);
-							} else {
-								args = [];
-							}
-							args.splice(0, 0, remoteAction);
-							args.push(function(result, event) {
-								vfr.handleResultWithPromise(result, event, nullok, deferred);
-							});
-							if (options) {
-								args.push(options);
-							}
-							Manager.invokeAction.apply(Manager, args);
-							return deferred.promise;
-						};
-					},
-					/**
-					 * Method returns an Angular promise as the product of a .send() prototyped method call
-					 * @param  {String}   result   Raw JSON string returned by js Remoting call
-					 * @param  {Object}   event    Status object returned from SF detailing errors, if any.
-					 * @param  {Boolean}  nullok   Can the result be null?
-					 * @param  {Deferred} deferred Angular Promise object
-					 * @return {Deferred}          Angular promise with resolution
-					 */
-					handleResultWithPromise: function(result, event, nullok, deferred) {
-						console.log('Handling with Promise');
-						console.log('result = ', result);
-						console.log('event = ', event);
-						console.log('null ok = ', nullok);
-						console.log('deferred = ', deferred);
-						if (result) {
-							if (typeof result !== 'object') {
-								result = JSON.parse(result);
-							}
-							if (Array.isArray(result) && result[0].message && result[0].errorCode) {
-								deferred.reject(result);
-								$rootScope.$safeApply();
-							} else {
-								deferred.resolve(result);
-								$rootScope.$safeApply();
-							}
-						} else if (typeof nullok !== 'undefined' && nullok) {
-							deferred.resolve();
-							$rootScope.$safeApply();
-						} else {
-							deferred.reject({
-								message: 'Null returned by RemoteAction not called with nullOk flag',
-								errorCode: 'NULL_RETURN'
-							});
-							$rootScope.$safeApply();
-						}
-					},
-					// Bulk Create
-					bulkCreate: function() {
-						this.send('ngForceController.bulkCreate', standardOptions, false);
-					},
-					// Bulk Update
-					bulkUpdate: function() {
-						this.send('ngForceController.bulkUpdate', standardOptions, false);
-					},
-					// Create
-					create: function() {
-						this.send('ngForceController.create', standardOptions, false);
-					},
-					// Clone
-					clone: function() {
-						this.send('ngForceController.sObjectKlone', standardOptions, false);
-					},
-					// Delete
-					del: function() {
-						this.send('ngForceController.del', standardOptions, true);
-					},
-					// Describe
-					describe: function() {
-						this.send('ngForceController.describe', standardOptions, false);
-					},
-					// Describe Field Set
-					describeFieldSet: function() {
-						this.send('ngForceController.describeFieldSet', standardOptions, false);
-					},
-					// Describe Picklist Values
-					describePicklistValues: function() {
-						this.send('ngForceController.getPicklistValues', standardOptions, false);
-					},
-					// Get Object Type
-					getObjectType: function() {
-						this.send('ngForceController.getObjType', standardOptions, false);
-					},
-					// Get Query Results as select2 data
-					getQueryResultsAsSelect2Data: function() {
-						this.send('ngForceController.getQueryResultsAsSelect2Data', standardOptions, false);
-					},
-					// Query
-					query: function() {
-						this.send('ngForceController.query', {
-							escape: false,
-							timeout: 30000
-						}, false);
-					},
-					// Query from Fieldset
-					queryFromFieldset: function() {
-						this.send('ngForceController.queryFromFieldSet', {
-							escape: false,
-							timeout: 30000
-						}, false);
-					},
-					// Retrieve a field list for a given object.
-					retrieve: function() {
-						this.send('ngForceController.retrieve', standardOptions, false);
-					},
-					// Search (SOSL)
-					search: function() {
-						this.send('ngForceController.search', standardOptions, false);
-					},
-					// Soql from Fieldset
-					soqlFromFieldSet: function() {
-						this.send('ngForceController.soqlFromFieldSet', standardOptions, false);
-					},
-					// Update
-					update: function() {
-						this.send('ngForceController.updat', standardOptions, true);
-					},
-					// Upsert
-					upsert: function() {
-						this.send('ngForceController.upser', standardOptions, true);
+				/*
+				 * Kevin o'Hara released premote, a nice lib for wrapping
+				 * visualforce remoting calls in a promise interface. this
+				 * function .send() is largely a gentle refactoring of his
+				 * work, found in "premote" here:
+				 *    https://github.com/kevinohara80/premote
+				 * such that it locks into the ng exec loop and utilizes
+				 * the angular $q service, itself based on the Q lib
+				 * Kevin uses.
+				 *
+				 * Returns a function that, when called, invokes the js
+				 * remoting method specified in this call.
+				 * @param  {String}   remoteAction class.methodName string representing the Apex className and Method to invoke
+				 * @param  {Object}   options      Ojbect containing at least the timeout and escaping options. Passed to Remoting call
+				 * @param  {Boolean}  nullok       Can this method return null and it be OK?
+				 * @return {Function}              Function engaged with the NG execution loop, making Visualforce remoting calls.
+				 */
+				function send(remoteAction, options, nullok) {
+					var namespace, controller, method;
+					var Manager = Visualforce.remoting.Manager;
+					var parts = remoteAction.split('.');
+					if (options && typeof options !== 'object') {
+						throw new Error('Options must be an object');
 					}
+					if (parts.length < 2) {
+						throw new Error('Invalid Remote Action specified. Use Controller.MethodName or $RemoteAction.Controller.MethodName');
+					} else {
+						if (parts.length === 3) {
+							namespace = parts[0];
+							controller = parts[1];
+							method = parts[2];
+						} else if (parts.length === 2) {
+							controller = parts[0];
+							method = parts[1];
+						}
+					}
+
+					return function() {
+						var deferred = $q.defer();
+						var args;
+						if (arguments.length) {
+							args = Array.prototype.slice.apply(arguments);
+						} else {
+							args = [];
+						}
+						args.splice(0, 0, remoteAction);
+						args.push(function(result, event) {
+							handleResultWithPromise(result, event, nullok, deferred);
+						});
+						if (options) {
+							args.push(options);
+						}
+						Manager.invokeAction.apply(Manager, args);
+						return deferred.promise;
+					};
+				};
+
+				/**
+				 * Method returns an Angular promise as the product of a .send() prototyped method call
+				 * @param  {String}   result   Raw JSON string returned by js Remoting call
+				 * @param  {Object}   event    Status object returned from SF detailing errors, if any.
+				 * @param  {Boolean}  nullok   Can the result be null?
+				 * @param  {Deferred} deferred Angular Promise object
+				 * @return {Deferred}          Angular promise with resolution
+				 */
+				function handleResultWithPromise(result, event, nullok, deferred) {
+					if (result) {
+						if (typeof result !== 'object') {
+							result = JSON.parse(result);
+						}
+						if (Array.isArray(result) && result[0].message && result[0].errorCode) {
+							deferred.reject(result);
+							$rootScope.$safeApply();
+						} else {
+							deferred.resolve(result);
+							$rootScope.$safeApply();
+						}
+					} else if (typeof nullok !== 'undefined' && nullok) {
+						deferred.resolve();
+						$rootScope.$safeApply();
+					} else {
+						deferred.reject({
+							message: 'Null returned by RemoteAction not called with nullOk flag',
+							errorCode: 'NULL_RETURN'
+						});
+						$rootScope.$safeApply();
+					}
+				};
+
+				/**
+				 * This the returned object literal for the $get call.
+				 * Note that send and standardOptions essentially punt to their parental
+				 * objects.
+				 *
+				 * Special thanks to @marpstar for helping me figure out the bug in .send()
+				 * that was causing pre-defined vfr methods to fail.
+				 *
+				 */
+				return {
+					send: send,
+					standardOptions: standardOpts,
+					// Bulk Create
+					bulkCreate: send('ngForceController.bulkCreate', this.standardOptions, false),
+					// Bulk Update
+					bulkUpdate: send('ngForceController.bulkUpdate', this.standardOptions, false),
+					// Create
+					create: send('ngForceController.create', this.standardOptions, false),
+					// Clone
+					clone: send('ngForceController.sObjectKlone', this.standardOptions, false),
+					// Delete
+					del: send('ngForceController.del', this.standardOptions, true),
+					// Describe
+					describe: send('ngForceController.describe', this.standardOptions, false),
+					// Describe Field Set
+					describeFieldSet: send('ngForceController.describeFieldSet', this.standardOptions, false),
+					// Describe Picklist Values
+					describePicklistValues: send('ngForceController.getPicklistValues', this.standardOptions, false),
+					// Get Object Type
+					getObjectType: send('ngForceController.getObjType', this.standardOptions, false),
+					// Get Query Results as select2 data
+					getQueryResultsAsSelect2Data: send('ngForceController.getQueryResultsAsSelect2Data', this.standardOptions, false),
+					// Query
+					query: send('ngForceController.query', {
+						escape: false,
+						timeout: 30000
+					}, false),
+					// Query from Fieldset
+					queryFromFieldset: send('ngForceController.queryFromFieldSet', {
+						escape: false,
+						timeout: 30000
+					}, false),
+					// Retrieve a field list for a given object.
+					retrieve: send('ngForceController.retrieve', this.standardOptions, false),
+					// Search (SOSL)
+					search: send('ngForceController.search', this.standardOptions, false),
+					// Soql from Fieldset
+					soqlFromFieldSet: send('ngForceController.soqlFromFieldSet', this.standardOptions, false),
+					// Update
+					update: send('ngForceController.updat', this.standardOptions, true),
+					// Upsert
+					upsert: send('ngForceController.upser', this.standardOptions, true)
 				};
 			}
 		};
